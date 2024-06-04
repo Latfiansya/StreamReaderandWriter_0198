ddf9c881748dc916d7a58f88a59fd277c0540de6
Latfiansya <latfiansyadzikri@gmail.com>
2024-06-04 10:49:29 +0700

    library untuk obyek array yang akan digunakan

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <exception>
 //untuk obyek exception yang akan digunakan
-#include <array>
+#include <array> 
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
