5ab32c8affd3bef4d849f82be1e7ad26eb30d4d9
Latfiansya <latfiansyadzikri@gmail.com>
2024-06-04 10:57:21 +0700

    mengeksekusi blok catch pertama

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
-        throw 0.5; //melemparkan sebuah integer maka
+        //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
     catch (int a) {
