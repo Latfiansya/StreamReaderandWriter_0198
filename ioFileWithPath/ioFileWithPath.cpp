c31d638b476e1a0cc772b930c9b8aa4b1665fad9
Latfiansya <latfiansyadzikri@gmail.com>
2024-06-04 10:41:36 +0700

    menutup file setelah menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -28,7 +28,7 @@ int main()
         //menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
-    //selesai dalam menulis sekarng tutup filenya
+    //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
 
