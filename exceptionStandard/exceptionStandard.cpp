b0c5c17f54c03f77e6dc79d26dbd1e5ab0bb1cbb
Latfiansya <latfiansyadzikri@gmail.com>
2024-06-04 10:49:03 +0700

    library untuk obyek exception yang akan digunakan

---
+++ exceptionStandard/exceptionStandard.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2 };
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris program yang terakhir" << endl;
+    /*Penanda 2: Bahwa Program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
+}
